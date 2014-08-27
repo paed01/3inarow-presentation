7f0f9e7a76baf4ed44d08ab8e3ceaa889603298b
Pål Edman <pal.edman@gmail.com>
2014-08-27 15:51:26 +0200

    Game over functionality

--- index.js
+++ index.js
@@ -21,51 +21,135 @@ var io = require('socket.io')(server);
 
 function createBoard() {
     var board = {};
-    for (var c = 1; c < 4; c++) {
-        for (var r = 1; r < 4; r++) {
-            board[r * 10 + c] = {
-                row: r,
-                col: c,
-                votes: 0
-            };
-        }
+    // for (var c = 1; c < 4; c++) {
+    //     for (var r = 1; r < 4; r++) {
+    //         board[r * 10 + c] = {
+    //             row: r,
+    //             col: c,
+    //             votes: 0
+    //         };
+    //     }
+    // }
+
+    for (var i = 1; i < 257; i *= 2) {
+        board[i] = {
+            id: i,
+            votes: 0
+        };
+        console.log('#createBoard', i)
     }
+
+
     return board;
 }
 var board = createBoard();
 
-function getMaxVote(board, callback) {
-    var b, v = 0;
-    for (var cell in board) {
-        if (board[cell].votes > v) {
-            b = board[cell];
-            v = b.votes;
-        } else {
-            board[cell].votes = 0;
+var sign = 'X';
+
+var three = [7, 56, 73, 84, 146, 273, 292, 448];
+
+function streak(checksum) {
+    var streak = [];
+    var b = checksum.toString(2);
+    var str = new Array(b.length + 1).join('0');
+    for (var i = 0; i < b.length; i++) {
+        var comp = str;
+        if (b[i] === '1') {
+            comp[i] = b[i];
+
+            console.log(comp);
+
+            streak.push(parseInt(comp, 2));
+            break;
         }
     }
-    if (b) {
-        b.checked = 'X';
+
+    return streak;
+}
+
+function getMaxVote(board, callback) {
+    var b, checked = {
+            X: 0,
+            O: 0
+        },
+        v = 0;
+    for (var cell in board) {
+        var ch = board[cell];
+
+        if (!ch.checked) {
+            if (ch.votes > v) {
+                if (b) {
+                    b.votes = 0;
+                }
+
+                b = ch;
+                v = b.votes;
+            } else {
+                ch.votes = 0;
+            }
+        } else {
+            checked[ch.checked] += ch.id;
+        }
     }
 
-    return callback(null);
+    var game;
+    if (b) {
+        b.checked = sign;
+        var sum = checked[b.checked] + b.id;
+        switch (sign) {
+            case 'X':
+                sign = 'O';
+                break;
+            case 'O':
+                sign = 'X';
+                break;
+            default:
+        }
+
+        for (var t in three) {
+            var check = three[t];
+            if ((sum & check) === check) {
+                console.log('game', sum, check);
+
+                game = {};
+                game.over = true;
+                game.winner = b.checked;
+                game.streak = streak(check);
+
+                break;
+            }
+        }
+    }
+
+    return callback(null, game);
 }
 
 io.on('connection', function(socket) {
-    socket.emit('game', board);
+    socket.emit('game', {
+        board: board
+    });
 
     socket.on('vote', function(data) {
-        var key = ~~data.row * 10 + ~~data.col;
-        board[key].votes++;
-        io.sockets.emit('vote', board[key]);
+        console.log(data);
+
+        board[data.id].votes++;
+        io.sockets.emit('vote', board[data.id]);
     });
 });
 
 server.listen(8080, function() {
-    var set = 10000;
+    var set = 3000;
     var timer = setInterval(function() {
-        getMaxVote(board, function(err, b) {
-            io.sockets.emit('game', board);
+        getMaxVote(board, function(err, game) {
+            console.log(board, game);
+
+            io.sockets.emit('game', {
+                board: board,
+                game: game
+            });
+            if (game) {
+                board = createBoard();
+            }
         });
-    }, set)
+    }, set);
 });
