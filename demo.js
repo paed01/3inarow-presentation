71edfa12200b8e13b2dc3f5e09422ecdb34c8bc0
Pål Edman <pal.edman@gmail.com>
2015-05-05 18:24:25 +0200

    Online game

--- demo.js
+++ demo.js
@@ -1,20 +1,33 @@
 var http = require('http');
 var filesystem = require('fs');
 var url = require('url');
+var Browserify = require('browserify');
+
+var IO = require('socket.io');
+
+var browserify = Browserify();
+var jsBundle = browserify.require('./lib/game.js', {
+    expose: 'game'
+});
 
 var server = http.createServer(function(req, res) {
   var reqPath = url.parse(req.url).pathname;
-
-  console.log(reqPath);
 
   var readstream;
   if (reqPath === '/'){
     readstream = filesystem.createReadStream('./public/index_local.html');
     return readstream.pipe(res);
+  } else if (reqPath === '/js/game.js') {
+    return jsBundle.bundle().pipe(res);
   }
 
   var filePath = './public' + reqPath;
   readstream = filesystem.createReadStream(filePath);
+
+  readstream.on('error', function() {
+    res.writeHead(404);
+    return res.end();
+  });
 
   if (reqPath.indexOf('/js/') === 0) {
     res.writeHead(200, { 'Content-type': 'application/json'});
@@ -26,3 +39,25 @@ var server = http.createServer(function(req, res) {
 server.listen(8080, function(err) {
   console.log('Server started', err);
 });
+
+var io = IO(server);
+
+io.on('connection', function(socket) {
+  socket.emit('hello', { and: { welcome: 'to IT-huset' }});
+
+  socket.on('check', function(data) {
+    console.log('Checked', data);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
